0f0932a4bce62e159e3a0f25f43f1af90c342cfe
武双 <wushshsha@163.com>
2018-04-10 19:05:41 +0800

    创建了文章和文章表单

---
+++ common/config/blog.sql
@@ -0,0 +1,118 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : flipbook.com
+ Source Server Type    : MySQL
+ Source Server Version : 50721
+ Source Host           : flipbook.com
+ Source Database       : blog
+
+ Target Server Type    : MySQL
+ Target Server Version : 50721
+ File Encoding         : utf-8
+
+ Date: 04/10/2018 19:04:39 PM
+*/
+
+SET NAMES utf8;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+--  Table structure for `admin`
+-- ----------------------------
+DROP TABLE IF EXISTS `admin`;
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增ID',
+  `username` varchar(255) NOT NULL COMMENT '用户名',
+  `auth_key` varchar(32) NOT NULL COMMENT '自动登录key',
+  `password_hash` varchar(255) NOT NULL COMMENT '加密密码',
+  `password_reset_token` varchar(255) DEFAULT NULL COMMENT '重置密码token',
+  `email_validate_token` varchar(255) DEFAULT NULL COMMENT '邮箱验证token',
+  `email` varchar(255) NOT NULL COMMENT '邮箱',
+  `role` smallint(6) NOT NULL DEFAULT '10' COMMENT '角色等级',
+  `status` smallint(6) NOT NULL DEFAULT '10' COMMENT '状态',
+  `avatar` varchar(255) DEFAULT NULL COMMENT '头像',
+  `vip_lv` int(11) NOT NULL DEFAULT '0' COMMENT 'vip等级',
+  `created_at` int(11) NOT NULL COMMENT '创建时间',
+  `updated_at` int(11) NOT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='管理员表\n';
+
+-- ----------------------------
+--  Table structure for `cats`
+-- ----------------------------
+DROP TABLE IF EXISTS `cats`;
+CREATE TABLE `cats` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增id',
+  `cat_name` varchar(255) DEFAULT NULL COMMENT '分类名称',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='分类表';
+
+-- ----------------------------
+--  Table structure for `posts`
+-- ----------------------------
+DROP TABLE IF EXISTS `posts`;
+CREATE TABLE `posts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增id',
+  `title` varchar(255) DEFAULT NULL COMMENT '标题',
+  `summary` varchar(255) DEFAULT NULL COMMENT '摘要',
+  `content` text COMMENT '内容',
+  `label_img` varchar(255) DEFAULT NULL COMMENT '标签图',
+  `cat_id` int(11) DEFAULT NULL COMMENT '分类id',
+  `user_id` int(11) DEFAULT NULL COMMENT '用户id',
+  `user_name` varchar(255) DEFAULT NULL COMMENT '用户名',
+  `is_valid` tinyint(4) DEFAULT '0' COMMENT '是否有效： 0----未发布，1---已发布',
+  `created_at` int(11) DEFAULT NULL COMMENT '创建时间',
+  `updated_at` int(11) DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`),
+  KEY `cat` (`cat_id`),
+  KEY `valid` (`is_valid`),
+  KEY `cat_id` (`cat_id`,`is_valid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='文章主表';
+
+-- ----------------------------
+--  Table structure for `relation_post_tags`
+-- ----------------------------
+DROP TABLE IF EXISTS `relation_post_tags`;
+CREATE TABLE `relation_post_tags` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增id',
+  `post_id` int(11) DEFAULT NULL COMMENT '文章id',
+  `tag_id` int(11) DEFAULT NULL COMMENT '标签id',
+  PRIMARY KEY (`id`),
+  KEY `post_id` (`post_id`,`tag_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='文章和标签关联表';
+
+-- ----------------------------
+--  Table structure for `tags`
+-- ----------------------------
+DROP TABLE IF EXISTS `tags`;
+CREATE TABLE `tags` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增id',
+  `tag_name` varchar(255) DEFAULT NULL COMMENT '标签名称',
+  `post_num` int(11) DEFAULT '0' COMMENT '关联文章数',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `tag_name` (`tag_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='标签表';
+
+-- ----------------------------
+--  Table structure for `user`
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增ID',
+  `username` varchar(255) NOT NULL COMMENT '用户名',
+  `auth_key` varchar(32) NOT NULL COMMENT '自动登录key',
+  `password_hash` varchar(255) NOT NULL COMMENT '加密密码',
+  `password_reset_token` varchar(255) DEFAULT NULL COMMENT '重置密码token',
+  `email_validate_token` varchar(255) DEFAULT NULL COMMENT '邮箱验证token',
+  `email` varchar(255) NOT NULL COMMENT '邮箱',
+  `role` smallint(6) NOT NULL DEFAULT '10' COMMENT '角色等级',
+  `status` smallint(6) NOT NULL DEFAULT '10' COMMENT '状态',
+  `avatar` varchar(255) DEFAULT NULL COMMENT '头像',
+  `vip_lv` int(11) NOT NULL DEFAULT '0' COMMENT 'vip等级',
+  `created_at` int(11) NOT NULL COMMENT '创建时间',
+  `updated_at` int(11) NOT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COMMENT='用户表\n';
+
+SET FOREIGN_KEY_CHECKS = 1;
---
+++ frontend/controllers/base/BaseController.php
@@ -0,0 +1,14 @@
+<?php
+namespace frontend\controllers\base;
+
+use yii\web\Controller;
+class BaseController extends Controller
+{
+    public function beforeAction($action)
+    {
+        if(!parent::beforeAction($action)){
+            return false;
+        }
+        return true;
+    }
+}
